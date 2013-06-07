6a33ff44b522a71789c226380b561a62e0a27c89
Serge Wazuki <serge.wazuki@gmail.com>
2013-06-07 17:57:42 +0700

    Updates
    - Design command bar.
    - Move custom scripts to separate folder.

---
+++ WebApp/Scripts/Controls/CommandBar.js
@@ -0,0 +1,14 @@
+﻿function StepOrientation(displayText, orientation) {
+    this.DisplayText = ko.observable(displayText);
+    this.Orientation = ko.observable(orientation);
+}
+
+function CommandBarViewModel() {
+    // Initializes dummy data
+    this.OrientationsArray = [
+        new StepOrientation("Horizontal", "hoz"),
+        new StepOrientation("Vertical", "var")];
+
+    // Bindings
+    this.Orientations = ko.observableArray(this.OrientationsArray);
+}
