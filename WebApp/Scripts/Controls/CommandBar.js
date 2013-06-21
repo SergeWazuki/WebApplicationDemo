da8d69119f374a628027a1c31ab907e8f5a5b13a
Serge Wazuki <serge.wazuki@gmail.com>
2013-06-21 17:19:59 +0700

    Updates

--- WebApp/Scripts/Controls/CommandBar.js
+++ WebApp/Scripts/Controls/CommandBar.js
@@ -1,14 +1,9 @@
-﻿function StepOrientation(displayText, orientation) {
-    this.DisplayText = ko.observable(displayText);
-    this.Orientation = ko.observable(orientation);
-}
+﻿var StepOrientation = new Enum({
+    Horizontal: 0,
+    Vertical: 1
+});
 
 function CommandBarViewModel() {
-    // Initializes dummy data
-    var orientationsArray = [
-        new StepOrientation("Horizontal", "hoz"),
-        new StepOrientation("Vertical", "var")];
-
     // Bindings
-    this.Orientations = ko.observableArray(orientationsArray);
+    this.Orientation = ko.observable(StepOrientation);
 }
